bb2a3b378e997b21ce67cc42ffb2c94057afd917
gprogrammin <87392072+NothingButProgrammer@users.noreply.github.com>
2021-10-30 17:26:36 +0300

    The program.
    the v1.0 of the program.

---
+++ ginstaller.sh
@@ -0,0 +1,34 @@
+############################################################GINSTALLER##########################################################   
+##############################################made by gprogrammer, fork me on github!###########################################   
+################################################################################################################################   
+  
+echo "  ________ .__                   __           .__   .__                   
+ /  _____/ |__|  ____    _______/  |_ _____   |  |  |  |    ____ _______  
+/   \  ___ |  | /    \  /  ___/\   __\\__  \  |  |  |  |  _/ __ \\_  __ \ 
+\    \_\  \|  ||   |  \ \___ \  |  |   / __ \_|  |__|  |__\  ___/ |  | \/ 
+ \______  /|__||___|  //____  > |__|  (____  /|____/|____/ \___  >|__|    
+        \/          \/      \/             \/                  \/         
+                                                                          "                                                  
+echo "Hello $(whoami)! welcome to ginstaller! (yea, gman inspired)"                                                                
+echo "for use in debian based systems, with apt installed!"                                                                        
+TODAY=$(date)                                                                                                                      
+echo "Current time: $date"                                                                                                         
+echo "packages included: neofetch, htop, cmatrix, nano, vim, cmatrix, python3, wget, snapd, git, tmux"                                   
+echo "don't like the packages in this autoinstaller? Fork me on github."                                                           
+#there will be more packages in later versions, sorry                                                                              
+#this is just a beta                                                                                                               
+echo "[PRESS ANY KEY TO CONTINUE]" #yep this is a very strange way to continue without conditions but ok                          
+read continue                                                                                                                      
+sudo apt install neofetch                                                                                                          
+sudo apt install htop                                                                                                              
+sudo apt install cmatrix                                                                                                           
+sudo apt install nano                                                                                                              
+sudo apt install python3                                                                                                           
+sudo apt install vim                                                                                                               
+sudo apt install wget                                                                                                              
+sudo apt install git                                                                                         
+sudo apt install snapd                
+sudo apt install tmux                                                                          
+echo "installation has finished!"                                                                             
+                                                                                                                           
+                                                                                                                          
